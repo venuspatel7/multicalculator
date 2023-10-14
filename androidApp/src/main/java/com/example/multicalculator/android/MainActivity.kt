c909ed2281a94c65b940808ed01a3951b50d346c
VENUS PATEL <A00265063@mycambrian.ca>
2023-10-14 19:16:14 -0400

    third

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -2,23 +2,16 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.multicalculator.Greeting
-import android.widget.Button
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.Color
 
 
 class MainActivity : ComponentActivity() {
@@ -30,82 +23,78 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    CalcView()
                 }
             }
         }
     }
 }
+
+
+
+@Preview
 @Composable
-fun CalcView()
-{
-    val displayText by mutableStateOf("0")
+fun CalcView() {
+    val display = remember { mutableStateOf("0") }
+
     Column(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .fillMaxWidth()
-    ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-
+        modifier = Modifier.background(Color.LightGray))
+    {
+        Row {
+                CalcDisplay(display)
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
-          Column {
-              for (i in 7 downTo 1 step 3) {
-                  CalcRow(i, arrayOf(Button(), Button(), Button()), "display text")
-              }
-          }
-           Column {
+        Row {
 
-           }
         }
     }
-
 }
+
 @Composable
-fun CalcRow(display: MutableState<String>, starNum:Int, numButton: Int )
-{
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
     val endNum = startNum + numButtons
     Row(modifier = Modifier.padding(0.dp)) {
         for (i in startNum until endNum) {
             CalcNumericButton(i, display)
         }
     }
-
 }
-@Composable
-fun CalcDisplay(display: MutableState<String>)
-{
-
-}
-fun CalcNumericButton(number: Int, display: MutableState<String>)
-{
-        Button
-}
-@Composable
-fun CalcOperationButton( operation: String, display: MutableState<String>)
-{
-    Button(onClick = "", modifier = Modifier.padding(4.dp))
-
-}
-
 
 @Composable
-fun CalcEqualsButton(display: MutableState<String>) {
-    Button(
-        onClick = { displayValue = "0" },
-        modifier = Modifier.padding(4.dp)
+fun CalcDisplay(display: MutableState<String>) {
+    Text(
+        text = display.value,
+        modifier = Modifier
+            .height(50.dp)
+            .fillMaxWidth()
+            .padding(5.dp),
+        fontWeight = FontWeight.Bold
     )
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+fun CalcNumericButton(number: Int, display: MutableState<String>) {
+    Button(
+        onClick = { display.value += number.toString() },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = number.toString())
     }
 }
+
+@Composable
+fun CalcOperationButton(operation: String, display: MutableState<String>) {
+    Button(onClick = {}, modifier = Modifier.padding(4.dp)) {
+        Text(text = operation)
+    }
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = "=")
+    }
+}
+
